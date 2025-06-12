d9d90e8bf84dd0a86091d5ce76947fa0c0d68844
Johan Klingström <johanklingstrom@anotherblock.io>
2025-06-12 10:45:15 +0200

    dithering shader, post processing

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client';
-import App from './App';
+import App from './app';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) throw new Error('Failed to find the root element');
